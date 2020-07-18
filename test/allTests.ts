4a31a781d7a213885d7977baf20677ae73dc9e4f
Harald Niesche <harald@niesche.de>
2020-07-18 16:26:25 +0200

    unit test resolveAfter and rejectAfter

--- test/allTests.ts
+++ test/allTests.ts
@@ -2,9 +2,9 @@
 import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
 import * as mocha from 'mocha';
-import { timeoutPromise } from '../src/index';
+import { timeoutPromise, resolveAfter, rejectAfter } from '../src/index';
 
-let neverPromise = new Promise((rs,rj) => {});
+let neverPromise = new Promise((_rs,_rj) => {});
 
 chai.use(chaiAsPromised);
 
@@ -18,7 +18,7 @@ mocha.describe('timeout without fallback', async () => {
     const start = Date.now();
     return chai.expect(timeoutPromise(neverPromise, 100))
       .to.eventually.be.rejectedWith(Error)
-      .then(() => chai.expect(Date.now()+1).to.be.greaterThan(start+100).and.lessThan(start+110));
+      .then(() => chai.expect(Date.now()+1).to.be.gte(start+100).and.lte(start+110));
   });
   it("times out to rejection", () => {
     return chai.expect(timeoutPromise(neverPromise, 100)).to.eventually.be.rejectedWith(Error);
@@ -34,7 +34,7 @@ mocha.describe('timeout with fallback', async () => {
     const start = Date.now();
     return chai.expect(timeoutPromise(neverPromise, 100, {}))
       .to.eventually.be.an('object')
-      .then(() => chai.expect(Date.now()+1).to.be.greaterThan(start+100).and.lessThan(start+110));
+      .then(() => chai.expect(Date.now()+1).to.be.gte(start+100).and.lte(start+110));
   });
   it("times out to fallback", () => {
     return chai.expect(timeoutPromise(neverPromise, 100, '#fallback#')).to.eventually.be.equal('#fallback#');
@@ -42,5 +42,35 @@ mocha.describe('timeout with fallback', async () => {
   it("returns the first available value", () => (
     chai.expect(timeoutPromise(Promise.resolve('first'), 100, 'fallback')).to.eventually.be.equal('first')
   ));
+});
+
+mocha.describe('resolveAfter', async () => {
+  it("resolves after the expected time", () => {
+    const start = Date.now();
+    return chai.expect(resolveAfter(100, {}))
+      .to.eventually.be.an('object')
+      .then(() => chai.expect(Date.now()+1).to.be.gte(start+100).and.lte(start+110));
+  });
+  it("resolves to undefined without arg", () => {
+    const start = Date.now();
+    return chai.expect(resolveAfter(100))
+      .to.eventually.be.an('undefined')
+      .then(() => chai.expect(Date.now()+1).to.be.gte(start+100).and.lte(start+110));
+  });
+});
+
+mocha.describe('rejectAfter', async () => {
+  it("rejects after the expected time", () => {
+    const start = Date.now();
+    return chai.expect(rejectAfter(100, new Error('I am very very disappointed.')))
+      .to.eventually.be.rejectedWith(Error)
+      .then(() => chai.expect(Date.now()+1).to.be.gte(start+100).and.lte(start+110));
+  });
+  it("rejects to undefined without arg", () => {
+    const start = Date.now();
+    return chai.expect(rejectAfter(100))
+      .to.eventually.be.rejectedWith(undefined)
+      .then(() => chai.expect(Date.now()+1).to.be.gte(start+100).and.lte(start+110));
+  });
 });
 
