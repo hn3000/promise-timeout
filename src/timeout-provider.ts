d9613c432b059943769153c282b8ea9a71892bab
Harald Niesche <harald@niesche.de>
2020-07-18 16:25:45 +0200

    ignore coverage of check for unusual JS environments

--- src/timeout-provider.ts
+++ src/timeout-provider.ts
@@ -5,7 +5,7 @@ export interface ITimeoutProvider {
 
 let _defaultTimeoutProvider = null;
 
-
+/* istanbul ignore next */
 if (typeof setTimeout === 'function' && typeof clearTimeout === 'function') {
   _defaultTimeoutProvider = function(timeoutMillis: number, callback: () => void, ...args: any[]) {
     const timeout = setTimeout(callback, timeoutMillis, ...args);
